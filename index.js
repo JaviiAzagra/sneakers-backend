fb8ee016bca189194f79d094e6b0d88a9b5e0088
JaviiAzagra <javiazagra12@gmail.com>
2023-11-28 16:14:16 +0100

    ADD USERS

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ require("dotenv").config();
 const cors = require("cors");
 const connectDb = require("./src/utils/database/db");
 
-
 const db = require("./src/utils/database/db");
 const server = express();
-
 
 const indexRoutes = require("./src/api/routes/index.routes");
 const sneakerRoutes = require("./src/api/routes/sneakers.routes");
 const hoodieRoutes = require("./src/api/routes/hoodies.routes");
+const userRoutes = require("./src/api/routes/users.routes");
 
 const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT;
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -23,31 +21,34 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
-
 /* db.connectDb(DB_URL); */
-connectDb()
+connectDb();
 
-server.use(cors({
+server.use(
+  cors({
     origin: "*",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-
-server.use(express.json({limit: "5mb"}));
+server.use(express.json({ limit: "5mb" }));
 server.use(express.urlencoded({ extended: false }));
 
 server.use("/", indexRoutes);
 server.use("/sneakers", sneakerRoutes);
 server.use("/hoodies", hoodieRoutes);
+server.use("/users", userRoutes);
 
 server.use("*", (req, res) => {
-  const error = new Error('PATH NOT FOUND! 404');
+  const error = new Error("PATH NOT FOUND! 404");
   error.status = 404;
   return res.status(error.status).json(error.message);
 });
 
 server.use((error, req, res, next) => {
-  return res.status(error.status || 500).json(error.message || "unexpected error");
+  return res
+    .status(error.status || 500)
+    .json(error.message || "unexpected error");
 });
 
 server.listen(PORT || 3000, () => {
---
+++ src/api/models/users.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+    username: { type: String, unique: true, trim: true },
+    /* img: { type: String, required: true, trim: true },
+    rol: {type : String, enum: ["teachers", "education student", "other"]} */
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
