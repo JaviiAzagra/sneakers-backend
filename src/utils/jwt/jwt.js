9fe4e54da4618f1543b804156a9dd1a30b971351
JaviiAzagra <javiazagra12@gmail.com>
2023-12-03 01:53:48 +0100

    FEAT: added token expires

--- src/utils/jwt/jwt.js
+++ src/utils/jwt/jwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const generateSign = (id, email) => {
-  return jwt.sign({ id, email }, process.env.JWT_SECRET, {});
+  return jwt.sign({ id, email }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 const verifyJwt = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
